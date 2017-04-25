97d9590e36da08be3ca3897374e9683758522db4
Ernane Sena <ernane.sena@gmail.com>
2017-04-24 23:19:31 -0300

    Configuração inicial das rotas

--- src/app.js
+++ src/app.js
@@ -1,14 +1,10 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import routes from './routes';
 
 const app = express();
-app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.send('Hello World!'));
-app.get('/products', (req, res) => res.send([{
-  name: 'Default product',
-  description: 'product description',
-  price: 100
-}]));
+app.use(bodyParser.json());
+app.use('/', routes);
 
 export default app;
